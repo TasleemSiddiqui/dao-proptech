a4c99785790ef16cbcb7ded42b7b7982d8dcfda0
TasleemSiddiqui <154926573+TasleemSiddiqui@users.noreply.github.com>
2024-10-14 17:47:54 +0500

    first commit

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,104 @@
+import Image from "next/image";
+import React from "react";
+import logo from "@/public/footer-Dao-PropTech-secondary-logo-b@2x.png";
+import Link from "next/link";
+import { Facebook, Instagram, Linkedin, Twitter, Youtube } from "lucide-react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 border-t-2 border-b-2 lg:py-10">
+        {/* 1st column */}
+        <div className="p-4 space-y-3">
+          <Image
+            src={logo}
+            alt="company name"
+            className="lg:h-auto w-full"
+          ></Image>
+          <p className="text-sm text-muted-foreground leading-6">
+            DAO PropTech is a registered company with the Securities and
+            Exchange Commission of Pakistan and a certified member of the
+            Pakistan Software Houses Association for IT & ITeS (P@SHA).
+          </p>
+        </div>
+        {/* 2nd column */}
+        <div className="p-4 space-y-3">
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight font-sans text-gray-800 ">
+            Company
+          </h3>
+          <ul className="space-y-2 text-primary text-base tracking-wide font-thin">
+            <li>
+              <Link href={"/"}>About Us</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Bloc</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Learn</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Bounty Program</Link>
+            </li>
+          </ul>
+        </div>
+        {/* 3rd column */}
+        <div className="p-4 space-y-3">
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight font-sans text-gray-800 ">
+          Support
+          </h3>
+          <ul className="space-y-2 text-primary text-base tracking-wide font-thin">
+            <li>
+              <Link href={"/"}>Careers</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Contact Us</Link>
+            </li>
+            <li>
+              <Link href={"/"}>FAQ&apos;s</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Privacy Policy</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Terms and Conditions</Link>
+            </li>
+          </ul>
+        </div>
+        {/* 4th column */}
+        <div className="p-4 space-y-3">
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight font-sans text-gray-800 ">
+            Address
+          </h3>
+          <ul className="space-y-2">
+            <li className="flex  text-base   font-semibold wax-w-[300px] ">
+            Location:<span className="font-medium tracking-wide  ">17-A Business Bay,Sector F, DHA 1, Islamabad</span>
+            </li>
+            <li className="flex  text-base   font-semibold ">
+            Email:<span className="font-medium tracking-wide  flex-wrap ">info@daoproptech.com</span>
+            </li>
+            <li className="flex  text-base   font-semibold ">
+            Phone:<span className="font-medium tracking-wide  flex-wrap "> +(92) 314 326 7767</span>
+            </li>
+          </ul>
+
+
+          {/* socil links */}
+          <ul className="flex gap-x-3">
+            <li className="bg-gray-100 rounded-full p-2 text-primary w-fit" > <Facebook size={18}/></li>
+            <li className="bg-gray-100 rounded-full p-2 text-primary w-fit" > <Instagram size={18}/></li>
+            <li className="bg-gray-100 rounded-full p-2 text-primary w-fit" > <Linkedin size={18}/></li>
+            <li className="bg-gray-100 rounded-full p-2 text-primary w-fit" > <Twitter size={18}/></li>
+            <li className="bg-gray-100 rounded-full p-2 text-primary w-fit" > <Youtube size={18}/></li>
+          </ul>
+        </div>
+      </div>
+      <div className="w-full text-center py-10">
+        <span className="text-sm md:text-base text-gray-900">
+          © 2024 DAO PROPTECH. All rights reserved.
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,88 @@
+'use client'
+import React, { useState } from "react";
+import logo from "@/public/DAO-PropTech-Primary-logo@2x.png";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+
+const Header = () => {
+// sheet state management
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header className="flex justify-between items-center lg:px-36 md:px-24 px-14 py-5 ">
+        {/* logo */}
+      <Image
+        src={logo}
+        alt="proptech"
+        className="md:h-[3rem] h-[2rem] w-auto"
+      />
+      {/* large screen navigation */}
+      <ul className="hidden text-gray-800 lg:flex justify-center items-center gap-x-4 font-semibold text-base tracking-wide font-sans">
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/"}>About us</Link>
+        </li>
+        <li>
+          <Link href={"/"}>Project</Link>
+        </li>
+        <li>
+          <Link href={"/"}>Platform</Link>
+        </li>
+        <li>
+          <Link href={"/"}>Blog</Link>
+        </li>
+        <li>
+          <Link href={"/"}>Contact us</Link>
+        </li>
+        <li>
+          <Button> Sign up</Button>
+        </li>
+      </ul>
+
+      {/* mobile navigation */}
+      <div className="lg:hidden block">
+        <Sheet open={open} onOpenChange={setOpen}>
+          <SheetTrigger>
+            <Menu />
+          </SheetTrigger>
+          <SheetContent side={"right"}>
+            <ul className="lg:hidden flex flex-col font-semibold text-base tracking-wide font-sans text-gray-400 text-left space-y-6 py-10">
+              <li>
+                <Link href={"/"} onClick={() => setOpen(false)}>Home</Link>
+              </li>
+              <li>
+                <Link href={"/"} onClick={() => setOpen(false)}>About us</Link>
+              </li>
+              <li>
+                <Link href={"/"} onClick={() => setOpen(false)}>Project</Link>
+              </li>
+              <li>
+                <Link href={"/"} onClick={() => setOpen(false)}>Platform</Link>
+              </li>
+              <li>
+                <Link href={"/"} onClick={() => setOpen(false)}>Blog</Link>
+              </li>
+              <li>
+                <Link href={"/"} onClick={() => setOpen(false)}>Contact us</Link>
+              </li>
+              <li>
+                <Button className="w-full" onClick={() => setOpen(false)}>Sign up</Button>
+              </li>
+            </ul>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
